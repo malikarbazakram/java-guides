2a659b8a78f36d74dcaa7a7e4548bb931d7b41b3
malikarbazakram <malikarbazakram@gmail.com>
2023-09-28 16:36:55 +0500

    Completed the Service layer testing!

--- spring-boot-testing/spring-boot-testing/src/main/java/net/javaguides/springboot/service/EmployeeService.java
+++ spring-boot-testing/spring-boot-testing/src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -2,7 +2,17 @@ package net.javaguides.springboot.service;
 
 import net.javaguides.springboot.model.Employee;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
 
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> getEmployeeById(long id);
+
+    Employee updateEmployee(Employee employeeToUpdate);
+
+    void deleteEmployee(long id);
 }
--- spring-boot-testing/spring-boot-testing/src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
+++ spring-boot-testing/spring-boot-testing/src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -6,16 +6,19 @@ import net.javaguides.springboot.repository.EmployeeRepository;
 import net.javaguides.springboot.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+
     @Override
     public Employee saveEmployee(Employee employee) {
         Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
@@ -24,5 +27,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Optional<Employee> getEmployeeById(long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employeeToUpdate) {
+        return employeeRepository.save(employeeToUpdate);
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        employeeRepository.deleteById(id);
     }
 }
