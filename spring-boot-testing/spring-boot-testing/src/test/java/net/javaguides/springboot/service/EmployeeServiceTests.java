aa5f0b9a4e29390bcd9e22e86ba3e6993dfb750f
malikarbazakram <malikarbazakram@gmail.com>
2023-09-28 12:11:20 +0500

    Mocked the Dependencies using Mock and InjectMock Annotations

--- spring-boot-testing/spring-boot-testing/src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
+++ spring-boot-testing/spring-boot-testing/src/test/java/net/javaguides/springboot/service/EmployeeServiceTests.java
@@ -1,26 +1,38 @@
 package net.javaguides.springboot.service;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import net.javaguides.springboot.service.impl.EmployeeServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
-  import java.util.Optional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EmployeeServiceTests {
 
-
+    @Mock
     private EmployeeRepository employeeRepository;
-    private EmployeeService employeeService;
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+    private Employee employee;
 
     @BeforeEach
     public void setup() {
-        employeeRepository = Mockito.mock(EmployeeRepository.class);
-        employeeService = new EmployeeServiceImpl(employeeRepository);
-
+        //employeeRepository = Mockito.mock(EmployeeRepository.class);
+        //employeeService = new EmployeeServiceImpl(employeeRepository);
+        employee = Employee.builder().id(1L).firstName("Ramesh").lastName("Fadatare").email("ramesh@gmail.com").build();
     }
 
     // JUnit test for saveEmployee method
@@ -29,17 +41,11 @@ public class EmployeeServiceTests {
     @DisplayName("JUnit test for saveEmployee method")
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
         // given - preconditions or setup
-        Employee employee = Employee.builder()
-                .id(1L)
-                .firstName("Ramesh")
-                .lastName("Fadatare")
-                .email("ramesh@gmail.com")
-                .build();
 
-        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
-                .willReturn(Optional.empty());
 
-        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+
+        given(employeeRepository.save(employee)).willReturn(employee);
 
         System.out.println(employeeRepository);
         System.out.println(employeeService);
@@ -50,6 +56,32 @@ public class EmployeeServiceTests {
         System.out.println(savedEmployee);
         // then - verify the output
         Assertions.assertThat(savedEmployee).isNotNull();
+
+    }
+
+
+    // JUnit test for saveEmployee method
+    @Test
+    @Order(2)
+    @DisplayName("JUnit test for saveEmployee method WHICH THROWS Exception")
+    public void givenExisitingEmail_whenSaveEmployee_thenThrowsException() {
+        // given - preconditions or setup
+
+
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
+
+        //given(employeeRepository.save(employee)).willReturn(employee);
+
+        System.out.println(employeeRepository);
+        System.out.println(employeeService);
+
+        // when - actions or the behaviour that we are going test
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        // then - verify the output
+        verify(employeeRepository, never()).save(any(Employee.class));
 
     }
 }
